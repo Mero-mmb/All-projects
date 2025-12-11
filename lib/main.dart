4aaeef1bdf292d78f3f68db75f7f04ce9bc996fc
Mero-mmb <marohmahfoodh@gmail.com>
2025-12-11 20:52:51 +0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,15 @@
 import 'package:flutter/material.dart';
+import 'home_screen.dart';
 
 void main() {
-  runApp(MyInteractiveApp());
-}
-
-class MyInteractiveApp extends StatefulWidget {
-  @override
-  https://github.com/malkthere/mad_lesson01_2425.git createState() => _MyInteractiveAppState();
-}
-
-class _MyInteractiveAppState extends State<MyInteractiveApp> {
-  String message = 'Welcome to Flutter!';
-  int counter = 0;
-
-  void updateMessage() {
-    setState(() {
-      message = 'Button Pressed!';
-      counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Interactive UI'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(message),
-              Text("$counter"),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: updateMessage,
-                child: Text('Press Me'),
-              ),
-            ],
-          ),
-        ),
+  runApp(
+    MaterialApp(
+      title: 'تطبيق تتبع المصاريف',
+      theme: ThemeData(primarySwatch: Colors.teal, useMaterial3: true),
+      home: Directionality(
+        textDirection: TextDirection.rtl,
+        child: HomeScreen(),
       ),
-    );
-  }
+    ),
+  );
 }
